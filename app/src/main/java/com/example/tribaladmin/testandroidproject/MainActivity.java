eb0cf1f7a429529e1b59712251703062c97f5eea
TribalAdmin <eiwai2thin@gmail.com>
2015-10-14 18:07:29 +0800

    to test findbugs report.

--- app/src/main/java/com/example/tribaladmin/testandroidproject/MainActivity.java
+++ app/src/main/java/com/example/tribaladmin/testandroidproject/MainActivity.java
@@ -6,11 +6,18 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    int testValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String testString;
+
+    }
+
+    private void testFingBugs(){
+        String testFindBugs;
     }
 
 
